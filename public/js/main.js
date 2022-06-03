46d0e334fc2e647cc2fca7ac5e709245c6b22988
Luiz Claudio de Quadra <56608210+ldequadra@users.noreply.github.com>
2022-06-03 04:15:38 +0200

    Update main.js execution

--- public/js/main.js
+++ public/js/main.js
@@ -2,77 +2,63 @@
 window.addEventListener('load', () => {
     const loader = document.querySelector('.loader');
     loader.classList.add('loader--hidden');
+
     loader.addEventListener('transitionEnd', () => {
         document.body.removeChild(loader);
     });
 });
 
+/* ### Play audio fx ### */
+function playFX(audio) {
+    audio.currentTime = 0;
+    audio.play();
+};
+
+/* ### Set track number on display ### */
+function setTrackNumber(key) {
+    const array = [
+        'q', 'w', 'e', 'r', 't', 'y', 'u', 
+        'a', 's', 'd', 'f', 'g', 'h', 'j', 
+        'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+
+    var number = array.indexOf(key) + 1;
+    console.log(number);
+    const trackNumber = document.getElementById('trackNumber');
+
+    if (number < 10) {
+        trackNumber.textContent="0" + "0" + number;
+    } else {
+        trackNumber.textContent="0" + number;
+    }
+};
+
+/* ### Set track title on display ### */
+function setTrackTitle(key) {
+    const array = [
+        'q', 'w', 'e', 'r', 't', 'y', 'u', 
+        'a', 's', 'd', 'f', 'g', 'h', 'j', 
+        'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+
+    const titles = [
+        'QUE ISSO', 'SEM GRAÇA', 'POIN', 'POM', 'TOME', 'PARE', 'UUUII',
+        'AI', 'SOCO', 'DANÇA GATINHO', 'FAIL 1', 'ELE GOSTA', 'OH POTÊNCIA', 'JESUS',
+        'ABRLUABRLUA', 'XIII', 'CAVALO', 'DEMAIS', 'É BRINCADEIRA', 'NÃO', 'FAIL 2'
+    ];
+
+    var position = array.indexOf(key);
+    console.log(titles[position]);
+    const trackTitle = document.getElementById('trackTitle');
+    trackTitle.textContent = titles[position];
+};
+
+
 document.addEventListener("keydown",(event) => {
     var key = event.key;
     
-    audio = document.getElementById(key);
-    button = document.getElementById('button_' + key)
-    trackNumber = document.getElementById('trackNumber')
-    trackTitle = document.getElementById('trackTitle')
-    trackSubtitle = document.getElementById('trackSubtitle')
-
-    function playFX(audio) {
-        audio.currentTime = 0;
-        audio.play();
-    }
-    
-    
-    function setTrackNumber(key) {
-        array = [
-            'q', 'w', 'e', 'r', 't', 'y', 'u', 
-            'a', 's', 'd', 'f', 'g', 'h', 'j', 
-            'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-    
-        number = array.indexOf(key) + 1;
-        console.log(number);
-        trackNumber = document.getElementById('trackNumber');
-        if (number < 10) {
-            trackNumber.textContent="0" + "0" + number;
-        } else {
-            trackNumber.textContent="0" + number;
-        }
-    }
-    
-    function setTrackTitle(key) {
-        array = [
-            'q', 'w', 'e', 'r', 't', 'y', 'u', 
-            'a', 's', 'd', 'f', 'g', 'h', 'j', 
-            'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-    
-        titles = [
-            'QUE ISSO',
-            'SEM GRAÇA',
-            'POIN',
-            'POM',
-            'TOME',
-            'PARE',
-            'UUUII',
-            'AI',
-            'SOCO',
-            'DANÇA GATINHO',
-            'FAIL 1',
-            'ELE GOSTA',
-            'OH POTÊNCIA',
-            'JESUS',
-            'ABRLUABRLUA',
-            'XIII',
-            'CAVALO',
-            'DEMAIS',
-            'É BRINCADEIRA',
-            'NÃO',
-            'FAIL 2',
-        ];
-    
-        position = array.indexOf(key);
-        console.log(titles[position]);
-        trackTitle = document.getElementById('trackTitle');
-        trackTitle.textContent = titles[position];
-    }
+    //var button = document.getElementById('button_' + key)
+    var audio = document.getElementById(key);
+    const trackNumber = document.getElementById('trackNumber');
+    const trackTitle = document.getElementById('trackTitle');
 
     playFX(audio);
     setTrackNumber(key);
